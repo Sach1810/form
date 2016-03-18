c9be4e52cf366b71d3a5037f1b8480f1e04fd010
Sacha David <Sach1810@Sachas-MacBook-Pro.local>
2016-03-18 15:48:25 +0200

    All validation complete other than duplications which will come after feilds inputted as an object

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-var firstName = null;  
+var firstName = null;
+var lastName = null;
+var email = null;
+var username = null;
+var password = null;
+
+
+var specialCharacters = /^(?=.*[0-9_\W]).+$/;
 
 var validation = {
   firstName: "Please type your first name",
@@ -9,10 +16,17 @@ var validation = {
 };
 
 
-//********** Check for duplicate username and email **********
-// var duplicate = function duplicte(){
+// ********** Check for duplicate username and email **********
+var duplicate = function duplicte(){
 
-// };
+}; // ********** END OF DUPLICATE FUNCTION **********
+
+
+// ********** If everything is correct **********
+var success = function success(){
+
+}; // ********** END OF DUPLICATE FUNCTION **********
+
 
 //********** MAIN SUBMIT BUTTON FUNCTION **********
 var submitForm = function (){
@@ -26,6 +40,65 @@ var submitForm = function (){
   $('#firstNameError').empty();
 };
 
+  //***** Last Name *****
+  lastName = $('#lastNameInput').val();
+
+  if (lastName == null || lastName ==="") {
+    $('#lastNameError').text(validation.lastName);
+  } else {
+    $('#lastNameError').empty();
+  };
+
+  //***** Email ******
+  email = $('#emailInput').val();
+  var emailVerificationOne = email.indexOf('@');
+  var emailVerificationTwo = email.indexOf('.');
+
+  if (emailVerificationOne < 0 || emailVerificationTwo < 0) {
+    $('#emailError').text(validation.email);
+  } else {
+    $('#emailError').empty();
+  };
+
+  //***** Username *****
+  username = $('#usernameInput').val();
+
+  if (username.length < 5) {
+    $('#usernameError').text(validation.username);
+  } else {
+    $('#usernameError').empty();
+  };
+
+  //***** Repeat Password *****
+  var repeatPassword = $('#repeatPasswordInput').val();
+
+  if (repeatPassword !== password) {
+    $('#repeatPasswordError').text(validation.repeatPassword);
+  } else {
+    $('#repeatPasswordError').empty();
+  };
+
 }; // ********** END OF SUBMIT BUTTON FUNCTION **********
 
+// ********** Password live validation checker **********
+$(document).ready(function(){
+  $('#passwordInput').keyup(function(){
+        password = $('#passwordInput').val()
+        if (password.length < 7) {
+          console.log("less than 7");
+          $('#characters').css ("color","red");
+        } else if (password.length >=7) {
+          $('#characters').css ("color","green");
+        };
 
+        if (specialCharacters.test(password) == false) {
+          $('#special').css ("color","red");
+        } else {
+          $('#special').css ("color","green");
+
+        };
+      }
+    );
+  }
+);
+// ********** End password validation checker *********
